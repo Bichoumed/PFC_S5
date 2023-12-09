e345658e792e729e57525b4c17baae2f829b87e5
unknown <21056@supnum.mr>
2023-12-09 14:30:30 +0000

    l'environment du travail

---
+++ MianBase/src/main/java/mr/mizan/MianBase/MianBaseApplication.java
@@ -0,0 +1,13 @@
+package mr.mizan.MianBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MianBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MianBaseApplication.class, args);
+	}
+
+}
---
+++ MianBase/src/test/java/mr/mizan/MianBase/MianBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.mizan.MianBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MianBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
